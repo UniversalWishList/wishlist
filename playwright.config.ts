de76c40c5ac5f580afe4beff6bbaf3da02434714
hmcguire0583 <harry_mcguire@student.uml.edu>
2026-03-19 00:10:31 -0400

    updated playwright config to ignore jest tests (test)

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,6 +14,10 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
     testDir: "./tests",
     /* Run tests in files in parallel */
+    testIgnore: [
+        '/tests/apikeygen/'
+    ],
+    /* Ignore Jest tests directory */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
