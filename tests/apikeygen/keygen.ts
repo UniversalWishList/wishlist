026e6f0b1fb7c4ef97d7dd293d01296ede57a9f2
sykowsk <sykowskievan@gmail.com>
2026-04-08 12:31:01 -0400

    Add API key hashing function

--- tests/apikeygen/keygen.ts
+++ tests/apikeygen/keygen.ts
@@ -1,7 +1,13 @@
 import * as crypto from 'crypto';
+import * as bcrypt from 'bcrypt';
+
 export const generateApiKey = async () => {
   const randomBytes = crypto.randomBytes(32);
   const key = randomBytes.toString('hex');
   const prefix = 'uwl';
   return `${prefix}_${key}`;
+};
+
+export const hashApiKey = async (apiKey: string) => {
+  return await bcrypt.hash(apiKey, 12);
 };
